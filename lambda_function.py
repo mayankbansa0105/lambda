4df721aab276fef4cd6595d6d0ee2841dfb177f7
mayankbansa0105 <62702672+mayankbansa0105@users.noreply.github.com>
2024-07-11 11:11:45 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Fahim!')
+        'body': json.dumps('Hello from Fahim-new!')
     }
